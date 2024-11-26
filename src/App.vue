dbf6606a027de47484455ca254888199b5f16547
Tilman Hornung (t1h0) <64684735+t1h0@users.noreply.github.com>
2024-11-26 20:35:20 +0100

    Make menu shrinkable

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { inject } from 'vue'
+import { inject, onMounted, onUnmounted } from 'vue'
 const firstName = inject('firstName') as string
 const firstNameOfficial = inject('firstNameOfficial') as string
+
+// function parallax() {
+//   var parralaxElements = document.getElementsByClassName("parallax");
+//   var scrollAmount = window.scrollY / 100;
+//   Array.from(parralaxElements).forEach((el) => {
+//     el.style.top = - scrollAmount + "%";
+//   });
+// }
+
+onMounted(() => {
+  window.addEventListener("scroll", function () {
+    const header = document.getElementsByTagName("header");
+    const nav_classes = header[0].classList;
+    if (document.documentElement.scrollTop != 0) {
+      if (nav_classes.contains("shrink") === false) {
+        nav_classes.toggle("shrink");
+      }
+    }
+    else {
+      if (nav_classes.contains("shrink") === true) {
+        nav_classes.toggle("shrink");
+      }
+    }
+  })
+})
+
+// onUnmounted(() => {
+//   window.removeEventListener('scroll', parallax);
+// })
+
 </script>
 
 <template>
@@ -42,6 +72,20 @@ const firstNameOfficial = inject('firstNameOfficial') as string
       color: var(--color-text);
     }
   }
+
+  header {
+    padding: var(--app-padding);
+  }
+
+  .shrink {
+    font-size: 2rem !important;
+    line-height: 1 !important;
+    gap: 0 !important;
+    --small-padding: calc(var(--app-padding) / 2);
+    padding: var(--small-padding) var(--app-padding) var(--small-padding) var(--app-padding);
+    width: 100%;
+    box-shadow: 0 2.5px 0px var(--c-main);
+  }
 }
 
 #pageWrapper {
@@ -53,16 +97,22 @@ const firstNameOfficial = inject('firstNameOfficial') as string
 
 #contentWrapper {
   width: 100%;
+  padding: 0 var(--app-padding) 0 var(--app-padding);
 }
 
 header {
   display: flex;
+  position: sticky;
+  gap: 1rem;
+  top: 0;
   flex-direction: column;
   place-items: left;
   line-height: 1.5;
   font-size: 2.25rem;
   font-family: 'Arial Black' !important;
-  margin-bottom: 1.25rem;
+  background-color: var(--color-background);
+  // transitions
+  transition: font-size 1s, gap 1s, padding 1s, box-shadow 1s;
 }
 
 footer {
@@ -85,7 +135,6 @@ nav {
   display: flex;
   flex-direction: row;
   text-align: left;
-  margin-top: 1rem;
 
   a {
     padding: 0 0 0;
